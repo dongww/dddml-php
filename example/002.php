44ea2e42dc5b5e62e071782c1d14c7df95982a8c
dongww <dongww@gmail.com>
2016-05-24 18:34:11 +0800

    增加 PUT 演示

--- example/002.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * User: dongww
- * Date: 2016/5/21
- * Time: 12:19
- */
---
+++ example/put-001.php
@@ -0,0 +1,19 @@
+<?php
+use PhpGo\ApiService\ManagerFactory;
+use ValueObject\User\UserManager;
+use PhpGo\ApiService\Exception;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+/** @var ManagerFactory $ef */
+$ef = require __DIR__ . '/bootstrap.php';
+
+/** @var UserManager $userManager */
+$userManager = $ef->create('User');
+
+$user = $userManager->login(
+    '15658202878',
+    '555555'
+);
+
+var_dump($user);
