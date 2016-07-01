49ae251cc5dbda33137e4a6406be02e3be2e5ff5
dongww <dongww@gmail.com>
2016-07-01 15:56:44 +0800

    修正命名不匹配的问题

--- example/auth.php
+++ example/auth.php
@@ -1,12 +1,12 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$token = \Dddml\Auth::create(
+$auth = \Dddml\Auth::create(
     'http://testtenant1.wms.dddml.org:58760/oauth2/token',
     'test@dddml.org',
     '123456Abc!',
     '54fa1208384f4762a3ace231dbdbe88d'
 );
 
-var_dump($token);
-echo $token->getToken();
+var_dump($auth);
+echo $auth->getToken();
