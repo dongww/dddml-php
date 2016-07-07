e36a99d4b0e4b42164585121b06be808f97b8e94
dongww <dongww@gmail.com>
2016-07-07 16:22:30 +0800

    新增内置字段对应名称的 META 常量，以便开发人员扩展的时候查询这些字段的名称

--- src/Dddml/AbstractExecutor.php
+++ src/Dddml/AbstractExecutor.php
@@ -25,6 +25,17 @@ abstract class AbstractExecutor
     const METHOD_PATCH  = 'PATCH';
     const METHOD_DELETE = 'DELETE';
 
+    const META_VERSION      = 'version';
+    const META_ACTIVE       = 'active';
+    const META_DELETED      = 'deleted';
+    const META_CREATED_BY   = 'createdBy';
+    const META_CREATED_AT   = 'createdAt';
+    const META_UPDATED_BY   = 'updateBy';
+    const META_UPDATED_AT   = 'updateAt';
+    const META_REQUESTER_ID = 'requesterId';
+    const META_COMMAND_TYPE = 'commandType';
+    const META_COMMAND_ID   = 'commandId';
+
     protected $baseUri;
 
     /** @var  Serializer */
