ca0668bef552d7991f1131678bfa000bae2ed5c8
dongww <dongww@gmail.com>
2016-07-09 15:59:22 +0800

    新增 MetaData 对象，并更改相关代码

---
+++ src/MetaData/Role/RoleMetaData.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * User: dongww
+ * Date: 2016/7/9
+ * Time: 15:54
+ */
+namespace MetaData\Role;
+
+class RoleMetaData
+{
+    const META_VERSION      = 'version';
+    const META_ACTIVE       = 'active';
+    const META_DELETED      = 'deleted';
+    const META_CREATED_BY   = 'createdBy';
+    const META_CREATED_AT   = 'createdAt';
+    const META_UPDATED_BY   = 'updateBy';
+    const META_UPDATED_AT   = 'updateAt';
+    const META_REQUESTER_ID = 'requesterId';
+    const META_COMMAND_TYPE = 'commandType';
+    const META_COMMAND_ID   = 'commandId';
+}
