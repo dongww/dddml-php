0f5a88a713ea5a9f87efca506bb31bfa808b588f
dongww <dongww@gmail.com>
2016-07-09 15:11:40 +0800

    修正错误

--- src/Command/Role/RoleCommandBody.php
+++ src/Command/Role/RoleCommandBody.php
@@ -4,14 +4,13 @@
  * Date: 2016/7/9
  * Time: 13:20
  */
-
 namespace Command\Role;
 
-
+use Dddml\Command\CommandBodyInterface;
 use Dddml\Command\CommandTrait;
 use Entity\Role\RoleFieldsTrait;
 
-class RoleCommandBody
+class RoleCommandBody implements CommandBodyInterface
 {
     use RoleFieldsTrait, CommandTrait, IsPropertyRemovedTrait;
 }
